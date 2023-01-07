ff212737e36db03245e6d30fc1aae1dd958ad0ff
breakintods <nastasiia.karpenko@gmail.com>
2023-01-07 22:32:07 +0100

    trelliscope panel

---
+++ data_exploration.R
@@ -0,0 +1,43 @@
+data<-read.csv("Violence_Against_Women_Girls_Data.csv")
+
+library(tidyverse)
+
+# data exploration
+
+apply(data,2,class)
+unique(data$Country)
+unique(data$Survey.Year)
+
+#extract year of the survey
+substrRight <- function(x, n){
+  substr(x, nchar(x)-n+1, nchar(x))
+} #https://stackoverflow.com/questions/7963898/extracting-the-last-n-characters-from-a-string-in-r
+data<-data%>%mutate(Survey.Year = as.factor(substrRight(Survey.Year, 2)))
+unique(data$Survey.Year)
+
+# change "... " with ""
+data<-data%>%mutate(Question = substring(Question, 4))
+
+unique(data$Question)                    
+
+
+data_top_yr<-data%>%group_by(Question,Survey.Year,Gender,Country)%>%
+  summarise(Value = mean(as.numeric(Value),na.rm=T))%>%mutate(Question =
+                                                                as.factor(Question))
+
+# trelliscope panel            
+
+library(gridExtra)
+library(trelliscopejs)
+
+data_top_yr%>%ggplot(aes(x=factor(Survey.Year),y=Value))+
+  geom_point() +
+  theme_dark() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))+
+  labs(x='Year') +
+  labs(y='% of people who agree with the question') + 
+  facet_grid(Gender~Question)+
+  ggtitle("A husband is justified in hitting or beating his wife") +
+  facet_trelliscope(~ Country, as_plotly = T, height = 720, width = 1380,
+                    path = 'C:/Users/пк/Desktop/R_Git_Hub/violane_women_version_control_R')
+
